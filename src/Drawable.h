b9ceddf1f286eb5ee6cfa46684cd4c56cc3a270d
Adam Poper <64482829+AdamPoper@users.noreply.github.com>
2020-11-25 22:49:40 -0500

    Add files via upload

--- src/Drawable.h
+++ src/Drawable.h
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <AP2DGL/Entity.h>
 #include "LineBuffer.h"
+#include "../imgui/imgui.h"
 
 enum class Type
 {
-	LINE, QUAD, TRIANGLE, CIRCLE, POLYGON, IMAGE
+	LINE, QUAD, TRIANGLE, CIRCLE, CUSTOM, IMAGE
 };
 struct Drawable
 {
@@ -32,6 +33,68 @@ struct Triangle : public Drawable
 	ap::Triangle triangle;
 	~Triangle() { std::cout << "Triangle Deleted" << std::endl; }
 };
+struct CustomShape : public Drawable
+{
+	CustomShape()
+	{
+		vertexCount = 5;	// 5 verticies by default
+		vertices = new ap::Vertex[vertexCount];   
+		position = { 650.0f, 300.0f };
+		color = ap::Vec4f(1.0f, 0.0f, 1.0f, 1.0f);
+		reset();
+		type = Type::CUSTOM;
+	}
+	~CustomShape()
+	{
+		delete[] vertices;
+	}
+	CustomShape(const CustomShape& cs)
+	{
+		type = Type::CUSTOM;
+		vertexCount = cs.vertexCount;
+		if (vertices != nullptr)
+			delete[] vertices;
+		vertices = new ap::Vertex[vertexCount];
+		for (int i = 0; i < cs.vertexCount; i++)
+			vertices[i] = cs.vertices[i];		// deep copy the vertex data
+	}
+	void move()
+	{
+		for (int i = 0; i < vertexCount; i++)
+		{
+			vertices[i].position.x += movementVector.x;
+			vertices[i].position.y += movementVector.y;
+		}
+		position.x += movementVector.x;
+		position.y += movementVector.y;																											
+	}
+	void reset()
+	{
+		float degStep = 360.0f / (float)vertexCount;
+		float degrees = 0.0f;
+		float radius  = 100.0f;
+		for (int i = 0; i < vertexCount; i++)
+		{
+			vertices[i].position.x = position.x + (glm::cos(glm::radians(degrees)) * radius);
+			vertices[i].position.y = position.y + (glm::sin(glm::radians(degrees)) * radius);
+			vertices[i].color      = color;
+			degrees += degStep;
+		}
+	}
+	void update()
+	{
+		move();
+		movementVector = { 0.0f, 0.0f };		
+	}
+	int activeIndex = 0;
+	bool showPreview = false;
+	ap::Vertex* vertices = nullptr;
+	int vertexCount; // int because ImGui likes it that way
+	ap::Vec2f position;
+	ap::Vec2f movementVector;
+	ap::Vec4f color;
+};
+
 struct Image : public Drawable
 {
 	Image(const char* file)
@@ -45,6 +108,7 @@ struct Image : public Drawable
 	std::string filepath;
 	~Image() { std::cout << "Image deleted" << std::endl; }
 };
+
 struct PreviewConfiguration
 {
 	ImVec4 previewColor = ImVec4(1.0f, 0.0f, 1.0f, 1.0f);
@@ -55,6 +119,10 @@ struct PreviewConfiguration
 	Drawable* previewDrawable = nullptr;
 	float previewRotation = 0.0f;
 	std::string filepath;
+	float previewScale = 1.0f;
+	uint32_t previewVertexCount = 5.0f;
+	int activeVertex = 0.0f;
+	
 	void reset()
 	{
 		previewColor = ImVec4(1.0f, 0.0f, 1.0f, 1.0f);
@@ -64,17 +132,20 @@ struct PreviewConfiguration
 		previewSize[1] = 100.0f;
 		previewRadius = 25.0f;
 		previewRotation = 0.0f;
+		previewScale = 1.0f;
 		filepath.clear();
 		showPreview = false;
 		delete previewDrawable;
 		previewDrawable = nullptr;
+		previewVertexCount = 5.0f;
+		activeVertex = 0.0f;
 	}
 	~PreviewConfiguration() { delete previewDrawable; }
 };
 class SaveScreenShotData
 {
 public:
-	SaveScreenShotData()
+	SaveScreenShotData(ap::Window* win)
 		: m_size(ap::Vec2f()), m_start(ap::Vec2f())
 	{
 		for (ap::Vertex& v : m_verticies)
@@ -82,6 +153,7 @@ public:
 		definingArea = false;
 		areaDefined = false;
 		started = false;
+		m_win = win;
 	}
 	void setStartPosition(const ap::Vec2f& start)
 	{
@@ -113,12 +185,12 @@ public:
 	{
 		renderer.Draw(m_verticies.data(), m_verticies.size(), ap::LINE_LOOP);
 	}
-	const ap::Vec2f& getStart() const { return m_start; }
-	const ap::Vec2f& getSize() const  { return m_size;  }
+	const ap::Vec2f& getStart() const { return { m_start.x, m_win->getWindowSize().y - m_start.y }; }
+	const ap::Vec2f& getSize() const  { return m_size; }
 public:
 	bool definingArea, areaDefined, started;
 private:
 	std::array<ap::Vertex, 4> m_verticies;
 	ap::Vec2f m_start, m_size;
-
+	ap::Window* m_win;
 };
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,8 @@
 #include <AP2DGL/Renderer.h>
 #include <AP2DGL/API_Tools/Clock.h>
+#include <AP2DGL/API_Tools/FileDialog.h>
 
 #include "Drawable.h"
-#include "FileUtils.h"
 
 #include <memory>
 #include <vector>
@@ -35,8 +35,7 @@ ap::Vec2f convertVector2(float* f)
 	return { f[0], f[1] };
 }
 
-// returns std::variant because it might have to return more than one possible type later
-std::variant<std::string> ShowMainMenuBar(ap::Window* win, ap::Renderer& renderer, SaveScreenShotData& saveArea)
+void ShowMainMenuBar(ap::Window* win, ap::Renderer& renderer, SaveScreenShotData& saveArea, std::vector<Drawable*>& drawables)
 {
 	std::string filepath;
 	if (ImGui::BeginMainMenuBar())
@@ -45,7 +44,7 @@ std::variant<std::string> ShowMainMenuBar(ap::Window* win, ap::Renderer& rendere
 		{
 			if (ImGui::MenuItem("Save"))
 			{
-				filepath = SaveFile("Text Files\0*.txt\0Any File\0*.*\0", win);
+				filepath = ap::FileDialog::SaveFile(ap::FileDialog::DefaultFileFilter, win);
 				if (!saveArea.areaDefined)
 					renderer.ScreenShot(filepath.c_str());
 				else if (saveArea.areaDefined)
@@ -56,12 +55,11 @@ std::variant<std::string> ShowMainMenuBar(ap::Window* win, ap::Renderer& rendere
 				
 			}
 			if (ImGui::MenuItem("Save as"))
-			{
-				
+			{				
 			}
 			if (ImGui::MenuItem("Open..."))  
 			{
-				filepath = OpenFile("Text Files\0*.txt\0Any File\0*.*\0", win);
+				filepath = ap::FileDialog::OpenFile("Text Files\0*.txt\0Any File\0*.*\0", win);
 				if (!filepath.empty())
 					std::cout << filepath << std::endl;
 			}
@@ -70,7 +68,12 @@ std::variant<std::string> ShowMainMenuBar(ap::Window* win, ap::Renderer& rendere
 		}
 		if (ImGui::BeginMenu("Edit"))
 		{
-			if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+			if (ImGui::MenuItem("Undo", "CTRL+Z"))
+			{
+				delete drawables.back();
+				drawables.pop_back();
+				std::cout << "Size: " << drawables.size() << std::endl;
+			}
 			if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
 			ImGui::Separator();
 			if (ImGui::MenuItem("Cut", "CTRL+X")) {}
@@ -83,7 +86,6 @@ std::variant<std::string> ShowMainMenuBar(ap::Window* win, ap::Renderer& rendere
 		}
 		ImGui::EndMainMenuBar();
 	}
-	return filepath;
 }
 void defineSaveCanvasArea(SaveScreenShotData& saveAreaData, ap::Window& window)
 {
@@ -107,12 +109,18 @@ void defineSaveCanvasArea(SaveScreenShotData& saveAreaData, ap::Window& window)
 void updateEraseVerticies(std::vector<Drawable*>& drawables, ImVec4 clearcolor)
 {
 	for (int i = 0; i < drawables.size(); i++)
+	{
 		if (drawables[i]->type == Type::LINE)
 		{
 			Line* l = dynamic_cast<Line*>(drawables[i]);
 			if (l != nullptr && l->buffer.Type() == DrawType::ERASE)
 					l->buffer.SetColor(convertVector4(clearcolor));
 		}
+	}
+}
+void renderCustomShape(const CustomShape& cs, ap::Renderer& renderer)
+{
+	renderer.Draw(cs.vertices, cs.vertexCount, ap::PRIMITIVES::POLYGON);
 }
 void SubmitDrawableRenderData(Drawable* drawable, ap::Renderer& renderer)
 {
@@ -158,6 +166,13 @@ void SubmitDrawableRenderData(Drawable* drawable, ap::Renderer& renderer)
 				renderer.DrawQuad(img->image.getPosition(), img->image.getSize(), &img->texture, img->image.getRotation());									
 			return;
 		}
+		case Type::CUSTOM:
+		{
+			CustomShape* cs = dynamic_cast<CustomShape*>(drawable);
+			if(cs != nullptr)
+				renderCustomShape(*cs, renderer);
+			return;
+		}
 	}		
 }
 Quad* makeQuad(Drawable* prev)
@@ -200,13 +215,15 @@ Image* makeImage(Drawable* prev)
 	img->image.setSize(i->image.getSize());
 	return img;
 }
-void QuadUI(PreviewConfiguration& qc,float windowWidth)
+void QuadUI(PreviewConfiguration& qc, float windowWidth)
 {
 	ImGui::Checkbox("Init Quad Preview", &qc.showPreview);
 	ImGui::ColorEdit4("Quad Color", (float*)&qc.previewColor);
 	ImGui::SliderFloat2("Quad Position", qc.previewPosition, 0.0f, windowWidth);
+	ImGui::DragFloat2("Quad Position", qc.previewPosition, 1.0f, -20.0f, 20.0f);
 	ImGui::SliderFloat2("Quad Size", qc.previewSize, 0.0f, 1000.0f);
 	ImGui::SliderFloat("Quad Rotation", &qc.previewRotation, 0.0f, 360.0f);
+	ImGui::SliderFloat("Quad Scale", &qc.previewScale, 0.0f, 5.0f);	
 }
 void CircleUI(PreviewConfiguration& cc, float windowWidth)
 {
@@ -221,15 +238,17 @@ void TriangleUI(PreviewConfiguration& tc, float windowWidth)
 	ImGui::ColorEdit4("Triangle Color", (float*)&tc.previewColor);
 	ImGui::SliderFloat2("Triangle Position", tc.previewPosition, 0.0f, windowWidth);
 	ImGui::SliderFloat2("Triangle Size", tc.previewSize, 0.0f, 1000.0f);
+	ImGui::SliderFloat("Triangle Scale", &tc.previewScale, 0.0f, 5.0f);
 }
 void ImageUI(PreviewConfiguration& imgc, ap::Window* win)
 {	
 	ImGui::SliderFloat2("Image Position", imgc.previewPosition, 0.0f, win->getWindowSize().x);
 	ImGui::SliderFloat2("Image Size", imgc.previewSize, 0.0f, 1000.0f);
 	ImGui::SliderFloat("Image Rotation", &imgc.previewRotation, 0.0f, 360.0f);
+	ImGui::SliderFloat("Image Scale", &imgc.previewScale, 0.0f, 5.0f);
 	if (ImGui::Button("Load Image From File"))
 	{
-		std::string filepath = OpenFile("Text Files\0*.txt\0Any File\0*.*\0", win);
+		std::string filepath = ap::FileDialog::OpenFile("Text Files\0*.txt\0Any File\0*.*\0", win);
 		if (!filepath.empty())
 		{
 			imgc.filepath = filepath;
@@ -241,19 +260,34 @@ void ImageUI(PreviewConfiguration& imgc, ap::Window* win)
 		}
 	}
 }
+void CustomShapeUI(CustomShape& cs, ap::Window* win)
+{
+	float offset = 15.0f;
+	ImGui::Checkbox("Init Custom Shape Preview", &cs.showPreview);	
+	ImGui::DragFloat2("Master Position", (float*)&cs.movementVector, 1.0f, -20, 20);
+	if (ImGui::ColorEdit4("Master Color", (float*)&cs.color))
+		for (int i = 0; i < cs.vertexCount; i++)
+			cs.vertices[i].color = cs.color;
+	ImGui::SliderInt("Select Active Index", &cs.activeIndex, 0, cs.vertexCount-1);
+	ImGui::SliderFloat2("Vertex Position", (float*)&cs.vertices[cs.activeIndex].position, 0.0f, win->Width());
+	ImGui::ColorEdit4("Vertex Color", (float*)&cs.vertices[cs.activeIndex].color);
+	if (ImGui::SliderInt("Vertex Count", &cs.vertexCount, 0, 32))
+		cs.reset();
+}
 int main()
 {	
 	const char* glsl_version = "#version 330";  // will be added to AP2DGL some day
 	ap::Window window(WIDTH, HEIGHT, "window");
+	glfwSwapInterval(1);  // enable vsync so the application doesn't make the cpu run at full speed
 	ap::Renderer renderer(&window);	
-	std::vector<Drawable*> drawables;
-	std::vector<std::vector<Drawable*>::iterator> erasingLines;
+	std::vector<Drawable*> drawables;	
 	Line currentBuffer;
 	bool drawing = false;
-	ap::Clock drawClock; // to limit the amount of verticies the renderer has to draww
+	ap::Clock drawClock; // to limit the amount of verticies the renderer has to draw
 	ap::Texture penTexture("assets/textures/pen.png");
 	ap::Texture eraserTexture("assets/textures/eraser.png");
 	bool isDrawSelected = true;
+	bool isPenEnabled = true;
 	ap::Quad pen;
 	pen.setTexture(&penTexture);
 	pen.setSize(ap::Vec2f(40.0f, 40.0f));
@@ -267,17 +301,17 @@ int main()
 	ImGui_ImplOpenGL3_Init(glsl_version);
 	ImVec4 renderClearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 	ImVec4 userDrawColor    = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);	
-	enum ConfigSlot { QUAD, CIRCLE, TRIANGLE, IMAGE };
+	enum ConfigSlot { QUAD, CIRCLE, TRIANGLE, IMAGE, CUSTOM };
 	std::array<PreviewConfiguration, 5> configurations;	
 	std::vector<std::future<void>> futures;
-	SaveScreenShotData saveAreaData;
+	SaveScreenShotData saveAreaData(&window);
+	CustomShape shapePreview;
 	while (window.isOpen())
 	{		
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();		
-		std::variant<std::string> vardata = ShowMainMenuBar(&window, renderer, saveAreaData);
-		//ShowMainMenuBar(&window);
+		ShowMainMenuBar(&window, renderer, saveAreaData, drawables);
 		pen.setPosition({ window.getMousePosition().x + 17.0f, window.getMousePosition().y - 17.0f });  // +- 17 to offset the texture
 		glClearColor(renderClearColor.x, renderClearColor.y, renderClearColor.z, renderClearColor.w);
 		{
@@ -291,11 +325,21 @@ int main()
 				}
 			}
 			else
+			{
 				if (ImGui::Button("Draw"))
 				{
 					isDrawSelected = true;
 					pen.setTexture(&penTexture);
 				}
+			}
+			ImGui::SameLine();
+			if (isPenEnabled)
+			{
+				if (ImGui::Button("Disable Pen"))
+					isPenEnabled = false;
+			}
+			else if (ImGui::Button("Enable Pen"))
+				isPenEnabled = true;
 			
 			ImGui::Text("Adjust Drawing Width");
 			ImGui::SliderFloat("", &lineWidth, 0.0f, 10.0f);    			
@@ -303,7 +347,7 @@ int main()
 			ImGui::ColorEdit3("  ", (float*)&userDrawColor);
 			ImGui::Text("Adjust BackGround Clear Color");
 			if (ImGui::ColorEdit3(" ", (float*)&renderClearColor))  
-				futures.push_back(std::async(std::launch::async, [&] {updateEraseVerticies(drawables, renderClearColor); }));							
+				futures.push_back(std::async(std::launch::async, [&] { updateEraseVerticies(drawables, renderClearColor); }));							
 			// erasing works by literally updating the color of every vertex of every line used for erasing to the current render clear color
 			// Normally this would crash when there is a lot of drawables and lines because it was running on one thread and
 			// it couldn't handle it. Using std::async to move the updating of the verticies to a different thread frees up the main
@@ -318,8 +362,7 @@ int main()
 					qpc.previewDrawable = new Quad;
 					qpc.previewDrawable->type = Type::QUAD;					
 				}
-				else if (qpc.previewDrawable != nullptr 
-					&& qpc.showPreview)
+				else if (qpc.previewDrawable != nullptr && qpc.showPreview)
 				{
 					Quad* q = dynamic_cast<Quad*>(qpc.previewDrawable);
 					if (q != nullptr)
@@ -327,7 +370,8 @@ int main()
 						q->quad.setColor(convertVector4(qpc.previewColor));
 						q->quad.setPosition(convertVector2(qpc.previewPosition));
 						q->quad.setSize(convertVector2(qpc.previewSize));
-						q->quad.rotate(qpc.previewRotation);
+						q->quad.setSize({ q->quad.getSize().x * qpc.previewScale, q->quad.getSize().y * qpc.previewScale });
+						q->quad.rotate(qpc.previewRotation);						
 					}
 				}
 				if (ImGui::Button("Create Quad") && qpc.showPreview)
@@ -347,8 +391,7 @@ int main()
 					cpc.previewDrawable = new Circle;
 					cpc.previewDrawable->type = Type::CIRCLE;
 				}
-				else if (cpc.previewDrawable != nullptr
-					&& cpc.showPreview)
+				else if (cpc.previewDrawable != nullptr	&& cpc.showPreview)
 				{
 					Circle* c = dynamic_cast<Circle*>(cpc.previewDrawable);
 					if (c != nullptr)
@@ -369,14 +412,12 @@ int main()
 			{
 				PreviewConfiguration& tpc = configurations[ConfigSlot::TRIANGLE];
 				TriangleUI(tpc, window.getWindowSize().x);
-				if (tpc.previewDrawable == nullptr
-					&& tpc.showPreview)
+				if (tpc.previewDrawable == nullptr && tpc.showPreview)
 				{
 					tpc.previewDrawable = new Triangle;
 					tpc.previewDrawable->type = Type::TRIANGLE;
 				}
-				else if (tpc.previewDrawable != nullptr
-					&& tpc.showPreview)
+				else if (tpc.previewDrawable != nullptr && tpc.showPreview)
 				{
 					Triangle* t = dynamic_cast<Triangle*>(tpc.previewDrawable);
 					if (t != nullptr)
@@ -384,6 +425,7 @@ int main()
 						t->triangle.setColor(convertVector4(tpc.previewColor));
 						t->triangle.setPosition(convertVector2(tpc.previewPosition));
 						t->triangle.setSize(convertVector2(tpc.previewSize));
+						t->triangle.setSize({ t->triangle.getSize().x * tpc.previewScale, t->triangle.getSize().y * tpc.previewScale });
 					}
 				}
 				if (ImGui::Button("Create Triangle") && tpc.showPreview)
@@ -410,6 +452,7 @@ int main()
 						img->image.setSize(convertVector2(imgc.previewSize));
 						img->image.setPosition(convertVector2(imgc.previewPosition));
 						img->image.rotate(imgc.previewRotation);
+						img->image.setSize({ img->image.getSize().x * imgc.previewScale, img->image.getSize().y * imgc.previewScale });
 					}
 				}
 				if (ImGui::Button("Create Image") && imgc.showPreview)
@@ -418,13 +461,36 @@ int main()
 					drawables.push_back(img);
 					imgc.reset();
 				}
-			}					
+			}			
+			if (ImGui::CollapsingHeader("Create Custom Shape!"))
+			{
+				CustomShapeUI(shapePreview, &window);
+				
+				if (shapePreview.showPreview)
+				{
+					shapePreview.update();
+				}
+				if (ImGui::Button("Create Shape"))
+				{
+					CustomShape* cs = new CustomShape(shapePreview);
+					drawables.push_back(cs);
+					shapePreview.showPreview = false;
+					shapePreview.reset();
+				}
+			}
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 
 				1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			ImGui::End();
-		}		
+		}	
+		if ((ap::Window::isKeyPressed(ap::key::AP_KEY_LEFT_CONTROL) || ap::Window::isKeyPressed(ap::key::AP_KEY_RIGHT_CONTROL))
+			&& ap::Window::isKeyPressed(ap::key::AP_KEY_Z) && drawClock.GetElapsedTimeAsMilliseconds() >= 300.0f)
+		{
+			delete drawables.back();
+			drawables.pop_back();
+			drawClock.Restart();
+		}			
 		if (ap::Window::isMouseButtonPressed(ap::AP_MOUSE_BUTTON_LEFT) && !ImGui::IsAnyItemActive()
-			&& drawClock.GetElapsedTimeAsMilliseconds() >= 10.0f)
+			&& drawClock.GetElapsedTimeAsMilliseconds() >= 10.0f && isPenEnabled)
 		{					
 			currentBuffer.buffer.SetWidth(lineWidth);
 			drawing = true;
@@ -445,7 +511,7 @@ int main()
 			drawables.push_back(temp);			
 			currentBuffer.buffer.Clear();
 			drawing = false;
-		}		
+		}	
 		defineSaveCanvasArea(saveAreaData, window);		
 		ImGui::Render();
 		renderer.ClearRenderBuffer();				
@@ -454,7 +520,9 @@ int main()
 		currentBuffer.buffer.Draw(renderer);
 		for (auto& c : configurations)
 			if(c.showPreview)
-				SubmitDrawableRenderData(c.previewDrawable, renderer);	
+				SubmitDrawableRenderData(c.previewDrawable, renderer);
+		if (shapePreview.showPreview)
+			renderCustomShape(shapePreview, renderer);
 		if (saveAreaData.areaDefined || saveAreaData.definingArea)
 			saveAreaData.draw(renderer);
 		if (!ImGui::IsAnyWindowHovered())
